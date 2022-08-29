366903c3f761ea617fef6f9e79dfff71e823b003
RyanE3IT <107210762+RyanE3IT@users.noreply.github.com>
2022-08-29 14:55:39 -0400

    Update run.ps1

--- Exec-HuduM365ProcessTenant/run.ps1
+++ Exec-HuduM365ProcessTenant/run.ps1
@@ -77,17 +77,16 @@ try {
             }
         }
 
-        $CustomerLinks = "<div class=`"nasa__content`"> 
-	<div class=`"nasa__block`"><button class=`"button`" onclick=`"window.open('https://portal.office.com/Adminportal/Home#/servicehealth?CTID=$($customer.CustomerId)')`" ><h3><i class=`"fas fa-passport`">&nbsp;&nbsp;&nbsp;</i>Service Health</h3></button></div>
+       $CustomerLinks = "<div class=`"nasa__content`"> 
+		<div class=`"nasa__block`"><button class=`"button`" onclick=`"window.open('https://portal.office.com/Adminportal/Home#/servicehealth?CTID=$($customer.CustomerId)')`" ><h3><i class=`"fas fa-passport`">&nbsp;&nbsp;&nbsp;</i>Service Health</h3></button></div>
         <div class=`"nasa__block`"><button class=`"button`" onclick=`"window.open('https://portal.office.com/Partner/BeginClientSession.aspx?CTID=$($customer.CustomerId)&CSDEST=o365admincenter')`"><h3><i class=`"fas fa-cogs`">&nbsp;&nbsp;&nbsp;</i>M365 Admin Portal</h3></button></div>
         <div class=`"nasa__block`"><button class=`"button`" onclick=`"window.open('https://outlook.office365.com/ecp/?rfr=Admin_o365&exsvurl=1&delegatedOrg=$($Customer.DefaultDomainName)')`"><h3><i class=`"fas fa-mail-bulk`">&nbsp;&nbsp;&nbsp;</i>Exchange Admin Portal</h3></button></div>
         <div class=`"nasa__block`"><button class=`"button`" onclick=`"window.open('https://aad.portal.azure.com/$($Customer.DefaultDomainName)')`" ><h3><i class=`"fas fa-users-cog`">&nbsp;&nbsp;&nbsp;</i>Azure Active Directory</h3></button></div>
-		<div class=`"nasa__block`"><button class=`"button`" onclick=`"window.open('https://endpoint.microsoft.com/$($customer.DefaultDomainName)/')`"><h3><i class=`"fas fa-laptop`">&nbsp;&nbsp;&nbsp;</i>Endpoint Management</h3></button></td></div>
-									
-	<div class=`"nasa__block`"><button class=`"button`" onclick=`"window.open('https://$customer.DisplayName-admin.sharepoint.com')`"><h3><i class=`"fab fa-file`">&nbsp;&nbsp;&nbsp;</i>SharePoint Portal</h3></button></div>
+		<div class=`"nasa__block`"><button class=`"button`" onclick=`"window.open('https://endpoint.microsoft.com/$($customer.DefaultDomainName)/')`"><h3><i class=`"fas fa-laptop`">&nbsp;&nbsp;&nbsp;</i>Endpoint Management</h3></button></td></div>								
         <div class=`"nasa__block`"><button class=`"button`" onclick=`"window.open('https://admin.teams.microsoft.com/?delegatedOrg=$($Customer.DefaultDomainName)')`"><h3><i class=`"fas fa-users`">&nbsp;&nbsp;&nbsp;</i>Teams Portal</h3></button></div>
         <div class=`"nasa__block`"><button class=`"button`" onclick=`"window.open('https://portal.azure.com/$($customer.DefaultDomainName)')`"><h3><i class=`"fas fa-server`">&nbsp;&nbsp;&nbsp;</i>Azure Portal</h3></button></div>
-        <div class=`"nasa__block`"><button class=`"button`" onclick=`"window.open('https://security.microsoft.com/homepage?tid=$($customer.CustomerId)')`" ><h3><i class=`"fas fa-key`">&nbsp;&nbsp;&nbsp;</i>Security Portal</h3></button></div>
+        <div class=`"nasa__block`"><button class=`"button`" onclick=`"window.open('https://security.microsoft.com/homepage?tid=$($customer.CustomerId)')`" ><h3><i class=`"fas fa-user-shield`">&nbsp;&nbsp;&nbsp;</i>Microsoft 365 Defender</h3></button></div>
+		<div class=`"nasa__block`"><button class=`"button`" onclick=`"window.open('https://compliance.microsoft.com/homepage?tid=$($customer.CustomerId)')`" ><h3><i class=`"fas fa-handshake`">&nbsp;&nbsp;&nbsp;</i>Microsoft Purview</h3></button></div>
 		
 		</div>"
 
